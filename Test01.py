3e899321eae5f752ed79865d40b9412afefd5d60
Talib Jafri <TJ_Mac@Talibs-MacBook-Pro.local>
2019-11-17 21:52:41 -0600

    Initial Commit

---
+++ Test01.py
@@ -0,0 +1,20 @@
+number = 6
+
+
+#populate a list of students
+student_names = ["Mark,Katarina,Jessica,Connor,Ian"]
+
+#add a name to the list
+
+#student_names.append("Homer")
+
+#print (student_names [0])
+#print (student_names [1])
+
+for name in student_names:
+    print("student_name is {0}".format(name))
+
+x = 0
+for index in range (10):
+    x = x + 10
+    print("the value of X is {0}".format(x))
